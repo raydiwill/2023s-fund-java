94691515abffc881074a6f66f29b5e128a77e7fe
Présentation <tbr@tbr.com>
2023-07-19 10:19:17 +0200

    began String to integer conversion

--- person-csv-data/test/TestLoading.java
+++ person-csv-data/test/TestLoading.java
@@ -4,14 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class TestLoading {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
 
         List<Patient> patients = new ArrayList<>();
         List<String> lines = Files.readAllLines(new File("person-csv-data/patients.csv").toPath());
@@ -20,9 +23,14 @@ public class TestLoading {
 
         List<String> partsForFirstPatient = Arrays.asList(lines.get(1).split(";"));
         String rawSocialSecurityNumber = partsForFirstPatient.get(0);
+        rawSocialSecurityNumber = rawSocialSecurityNumber.replace("\"", "");
         int socialSecurityNumber = Integer.parseInt(rawSocialSecurityNumber);
 
         Patient patientZero = new Patient(socialSecurityNumber,);
+
+        SimpleDateFormat formatter =  new SimpleDateFormat("dd/MM/yyyy");
+        Date parsedDate = formatter.parse(partsForFirstPatient.get(6));
+
 
 
     }
