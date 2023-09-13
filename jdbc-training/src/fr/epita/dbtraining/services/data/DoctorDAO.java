1772ed3277c25e41d52eff697e65bb1be7842268
Présentation <tbr@tbr.com>
2023-09-13 15:38:26 +0200

    jdbc introduction

--- jdbc-training/src/fr/epita/dbtraining/services/data/DoctorDAO.java
+++ jdbc-training/src/fr/epita/dbtraining/services/data/DoctorDAO.java
@@ -3,10 +3,7 @@ package fr.epita.dbtraining.services.data;
 import fr.epita.dbtraining.datamodel.Doctor;
 import fr.epita.dbtraining.services.exceptions.SaveFailedException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DoctorDAO {
     public void save(Doctor doctor) throws SaveFailedException {
@@ -18,6 +15,15 @@ public class DoctorDAO {
             insertStatement.execute();
         } catch (SQLException e) {
             throw new SaveFailedException(e);
+        }
+    }
+    public /**/ search(/**/) throws SearchFailedException {
+        PreparedStatement selectStatement = connection.prepareStatement("SELECT doc_id , doc_name FROM  DOCTORS LIMIT 5");
+        ResultSet resultSet = selectStatement.executeQuery();
+        while (resultSet.next()) {
+            String id = resultSet.getString("doc_id");
+            String name = resultSet.getString("doc_name");
+            System.out.println("id:name =>" + id + ":" + name);
         }
     }
 }
